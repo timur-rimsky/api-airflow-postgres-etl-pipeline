4df0b4fe4598af424a65b3bc8583daa962d225a8
timur <rtimurb@mail.ru>
2026-07-07 16:09:20 +0300

    Add database cursor context manager

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,4 +1,7 @@
+from contextlib import contextmanager
+
 import psycopg2
+
 from src.utils.settings import get_db_config
 
 
@@ -7,3 +10,29 @@ def get_connection():
 
     return psycopg2.connect(**db_config)
 
+
+@contextmanager
+def get_db_cursor():
+    connection = None
+    cursor = None
+
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+
+        yield connection, cursor
+
+        connection.commit()
+
+    except Exception:
+        if connection is not None:
+            connection.rollback()
+
+        raise
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+        if connection is not None:
+            connection.close()
